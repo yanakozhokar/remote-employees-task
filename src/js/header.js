b2cab8adafbf96c6e7e410dbd7e7b9cbaba69b4a
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-05-06 15:33:13 +0300

    update: change header styles

--- src/js/header.js
+++ src/js/header.js
@@ -1,43 +1,6 @@
-const navMenuDropdownRef = document.querySelector('.nav-menu__dropdown');
-const navMenuDropdownItemsRef = document.querySelectorAll(
-  '.nav-menu__dropdown-item'
-);
+// const dropdownListRef = document.querySelector('.nav-menu__dropdown');
+// const dropdownItemsRef = dropdownListRef.children;
 
-const onDropdownMouseover = () => {
-  navMenuDropdownItemsRef.forEach(
-    item =>
-      item.classList.contains('is-hidden') && item.classList.remove('is-hidden')
-  );
-};
-
-const onDropdownMouseout = () => {
-  navMenuDropdownItemsRef.forEach(
-    item =>
-      item !== navMenuDropdownItemsRef[0] && item.classList.add('is-hidden')
-  );
-};
-
-navMenuDropdownRef.addEventListener('mouseover', onDropdownMouseover);
-navMenuDropdownRef.addEventListener('mouseout', onDropdownMouseout);
-
-// const onDropdownMouseover = () => {
-//   navMenuDropdownItemsRef.forEach(
-//     item =>
-//       item.classList.contains('nav-menu__dropdown-item--hidden') &&
-//       item.classList.replace(
-//         'nav-menu__dropdown-item--hidden',
-//         'nav-menu__dropdown-item'
-//       )
-//   );
-// };
-
-// const onDropdownMouseout = () => {
-//   navMenuDropdownItemsRef.forEach(
-//     item =>
-//       item !== navMenuDropdownItemsRef[0] &&
-//       item.classList.replace(
-//         'nav-menu__dropdown-item',
-//         'nav-menu__dropdown-item--hidden'
-//       )
-//   );
-// };
+// for (let i = 1; i < dropdownItemsRef.length; i += 1) {
+//   dropdownItemsRef[i].style = `top: ${dropdownItemsRef[0].clientHeight * i}`;
+// }
