b0fafa415571ef5459c8fb8ab3e945201fd855af
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-05-07 22:02:57 +0300

    fix: hero-slider fixed

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,48 +1,49 @@
-// const heroCarousel = document.querySelector('.hero__rewiews-list');
-// const heroCarouselItems = Array.from(
-//   document.querySelectorAll('.hero__rewiews-item')
-// );
+const heroCarousel = document.querySelector('.hero__rewiews-list');
+const heroCarouselItems = Array.from(
+  document.querySelectorAll('.hero__rewiews-item')
+);
 
-// let currentIndex = 0;
-// let isAnimating = false;
-// let timer;
+let currentIndex = 0;
+let isAnimating = false;
+let timer;
 
-// function goToSlide(index) {
-//   if (isAnimating) return;
+function goToSlide(index) {
+  if (isAnimating) return;
 
-//   isAnimating = true;
+  isAnimating = true;
 
-//   heroCarouselItems[currentIndex].classList.remove('active');
-//   currentIndex = index % heroCarouselItems.length;
-//   heroCarouselItems[currentIndex].classList.add('active');
+  heroCarouselItems[currentIndex].classList.remove('active');
+  currentIndex = index % heroCarouselItems.length;
+  heroCarouselItems[currentIndex].classList.add('active');
 
-//   const slideWidth = heroCarouselItems[currentIndex].offsetWidth;
-//   heroCarousel.style.transform = `translateX(-${slideWidth * currentIndex}px)`;
+  const slideWidth = heroCarouselItems[currentIndex].offsetWidth;
+  heroCarousel.style.transition = 'transform 1s ease';
+  heroCarousel.style.transform = `translateX(-${slideWidth * currentIndex}px)`;
 
-//   setTimeout(() => {
-//     isAnimating = false;
-//   }, 500);
-// }
+  setTimeout(() => {
+    isAnimating = false;
+  }, 500);
+}
 
-// function goToPrev() {
-//   goToSlide(currentIndex - 1);
-//   resetTimer();
-// }
+function goToPrev() {
+  goToSlide(currentIndex - 1);
+  resetTimer();
+}
 
-// function goToNext() {
-//   goToSlide(currentIndex + 1);
-//   resetTimer();
-// }
+function goToNext() {
+  goToSlide(currentIndex + 1);
+  resetTimer();
+}
 
-// function autoPlay() {
-//   timer = setInterval(() => {
-//     goToNext();
-//   }, 3000);
-// }
+function autoPlay() {
+  timer = setInterval(() => {
+    goToNext();
+  }, 3000);
+}
 
-// function resetTimer() {
-//   clearInterval(timer);
-//   autoPlay();
-// }
+function resetTimer() {
+  clearInterval(timer);
+  autoPlay();
+}
 
-// autoPlay();
+autoPlay();
