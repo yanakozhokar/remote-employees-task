cc3dbe44dd0ae8b6b0a70a71eeedffda577d5d0f
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-05-09 14:27:35 +0300

    feature: add main markup, styles and logic to review-section

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './js/header';
 import './js/hero';
 import './js/recommendation';
 import './js/sell';
+import './js/review';
--- src/js/header.js
+++
@@ -1,6 +0,0 @@
-// const dropdownListRef = document.querySelector('.nav-menu__dropdown');
-// const dropdownItemsRef = dropdownListRef.children;
-
-// for (let i = 1; i < dropdownItemsRef.length; i += 1) {
-//   dropdownItemsRef[i].style = `top: ${dropdownItemsRef[0].clientHeight * i}`;
-// }
