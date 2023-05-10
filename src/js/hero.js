b61c7eb0045f4195196b0670e81dbe023ba111ff
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-05-10 18:42:43 +0300

    update: hero-section completed

--- src/js/hero.js
+++ src/js/hero.js
@@ -2,15 +2,13 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick.min.js';
 import $ from 'jquery';
 
-// const slidesAmount = () => {
-//   if (window.innerWidth < 768) {
-//     return 1;
-//   } else if (window.innerWidth >= 768 && window.innerWidth < 1200) {
-//     return 2;
-//   } else {
-//     return 3;
-//   }
-// };
+const formRef = document.querySelector('.hero__form');
+
+const onFormSubmit = event => {
+  event.preventDefault();
+};
+
+formRef.addEventListener('submit', onFormSubmit);
 
 $('.hero__slider-list').slick({
   autoplay: true,
