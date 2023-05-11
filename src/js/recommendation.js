9e85c8a50b25f486fb3e4a938da660e92ac63835
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-05-11 15:37:55 +0300

    update: change some logic in recommendation-section

--- src/js/recommendation.js
+++ src/js/recommendation.js
@@ -2,7 +2,13 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick.min.js';
 import $ from 'jquery';
 
-const slidesAmount = () => {
+const recSliderListRef = document.querySelector('.recommendation__slider-list');
+const recSliderItemsRef = document.querySelectorAll(
+  '.recommendation__slider-item'
+);
+let slider;
+
+const getSlidesAmount = () => {
   if (window.innerWidth < 768) {
     return 1;
   } else if (window.innerWidth >= 768 && window.innerWidth < 1200) {
@@ -12,10 +18,46 @@ const slidesAmount = () => {
   }
 };
 
-$('.recommendation__slider-list').slick({
-  autoplay: true,
-  autoplaySpeed: 5000,
-  prevArrow: $('.recomendation__prev-btn'),
-  nextArrow: $('.recommendation__next-btn'),
-  slidesToShow: slidesAmount(),
+const updateSlider = () => {
+  const slidesToShow = getSlidesAmount();
+  if (slider) {
+    slider.slick('slickSetOption', 'slidesToShow', slidesToShow, true);
+  }
+
+  const marginLeft =
+    Math.round(
+      (recSliderListRef.clientWidth -
+        recSliderItemsRef[0].clientWidth * slidesToShow) /
+        slidesToShow
+    ) + 1;
+
+  recSliderItemsRef.forEach(
+    item => (item.style = `margin-left: ${marginLeft}px;`)
+  );
+};
+
+$(document).ready(() => {
+  slider = $('.recommendation__slider-list').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    prevArrow: $('.recomendation__prev-btn'),
+    nextArrow: $('.recommendation__next-btn'),
+    slidesToShow: getSlidesAmount(),
+    variableWidth: true,
+  });
+
+  window.addEventListener('resize', updateSlider);
 });
+
+const slidesToShow = getSlidesAmount();
+
+const marginLeft =
+  Math.round(
+    (recSliderListRef.clientWidth -
+      recSliderItemsRef[0].clientWidth * slidesToShow) /
+      slidesToShow
+  ) + 1;
+
+recSliderItemsRef.forEach(
+  item => (item.style = `margin-left: ${marginLeft}px;`)
+);
