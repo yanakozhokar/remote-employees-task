18d45bba8a61d77df81d47150ccd4cbeb945972a
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-05-12 22:50:48 +0300

    update: add logic for filtring slides in recommendation-section

--- src/js/recommendation.js
+++ src/js/recommendation.js
@@ -3,10 +3,32 @@ import 'slick-carousel/slick/slick.min.js';
 import $ from 'jquery';
 
 const recSliderListRef = document.querySelector('.recommendation__slider-list');
-const recSliderItemsRef = document.querySelectorAll(
-  '.recommendation__slider-item'
+const recSliderItemsRef = [
+  ...document.querySelectorAll('.recommendation__slider-item'),
+];
+const recFilterListRef = document.querySelector('.recommendation__filter-list');
+const houses = recSliderItemsRef.filter(item => item.dataset.type === 'house');
+const villas = recSliderItemsRef.filter(item => item.dataset.type === 'villa');
+const apartments = recSliderItemsRef.filter(
+  item => item.dataset.type === 'apartment'
 );
 let slider;
+
+const onFilterBtnClick = event => {
+  if (event.target.nodeName !== 'BUTTON') {
+    return;
+  } else if (event.target.dataset.filter === 'houses') {
+    recSliderListRef.innerHTML = houses.map(house => house.outerHTML).join('');
+  } else if (event.target.dataset.filter === 'villas') {
+    recSliderListRef.innerHTML = villas.map(villa => villa.outerHTML).join('');
+  } else if (event.target.dataset.filter === 'apartments') {
+    recSliderListRef.innerHTML = apartments
+      .map(apartment => apartment.outerHTML)
+      .join('');
+  }
+};
+
+recFilterListRef.addEventListener('click', onFilterBtnClick);
 
 const getSlidesAmount = () => {
   if (window.innerWidth < 768) {
